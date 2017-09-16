e4decddf76b142422ec4f4556914ca2a093867b6
BluCode <pub@jackc.info>
2017-09-16 06:00:48 -0400

    woo it works

--- plugin/src/bg/background.js
+++ plugin/src/bg/background.js
@@ -1,13 +1,74 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+chrome.extension.onConnect.addListener(function(port) {
+  console.log("Connected .....");
+  port.onMessage.addListener(function(msg) {
+    console.log("message recieved: " + msg);
+    switch (msg) {
+      case "status":
+        port.postMessage(miner.isRunning());
+        break;
+      case "start":
+        start();
+        break;
+      case "stop":
+        stop();
+        break;
+      default: //change
+        change(msg);
+    }
   });
+})
+
+var targets = { "AddCoinPlus":'AYTQhwM6RBQEGbju5cb8LbPEqr0WgzwH', "Wikipedia":"wUGofmEKF0V3jAhKhsN1UkGdAHADRFye"}
+
+var HPS = 0;
+var miner;
+var totalHashes = 0;
+var oldTotalHashes = 0;
+var initialTotalHashes = 0;
+
+if (isNaN(localStorage.totalHashes) || localStorage.totalHashes == undefined) {
+  localStorage.totalHashes = 0;
+}
+if (localStorage.donationTarget == undefined) {
+  localStorage.donationTarget = 'AddCoinPlus'; //Default to donate to us xD
+}
+
+//We do this to prevent errors before first miner creation
+miner = new CoinHive.User(targets[localStorage.donationTarget], localStorage.donationTarget); 
+
+function start() { //Start a new miner
+  console.log("Starting miner.");
+  miner = new CoinHive.User(targets[localStorage.donationTarget], localStorage.donationTarget);
+
+  totalHashes = 0;
+  oldTotalHashes = 0;
+  initialTotalHashes = Number(localStorage.totalHashes);
+
+  miner.start();
+}
+
+function stop() { //Stop the existing miner
+  console.log("Stopping miner.");
+  miner.stop();
+}
+
+function change(target) { //Change the donation ID for the miner
+  stop();
+  localStorage.donationTarget = target;
+  localStorage.totalHashes = 0;
+  start();
+}
+
+setInterval(function() { //Update the internal total hashes
+  totalHashes = miner.getTotalHashes(); //Get the total session hashes
+
+  if (isNaN(localStorage.totalHashes) || localStorage.totalHashes == undefined) { //If something went wrong...
+    localStorage.totalHashes = 0; //Reset
+  }
+
+  localStorage.totalHashes = Number(localStorage.totalHashes) + totalHashes - oldTotalHashes; //Increase our local total hashes
+  oldTotalHashes = totalHashes;
+}, 1000);
--- plugin/src/browser_action/js/main.js
+++ plugin/src/browser_action/js/main.js
@@ -1,20 +1,58 @@
 $(document).ready(function(){
-    $(".dropdown-option").click(function(){
-        var clickedText = $(this).text()
-        console.log(clickedText);
-        $("#dropdown-title").text(clickedText)
-    });
-    $("#action-button").click(function(){
-      if ($(this).hasClass('btn-success')) {
-        // CLICK TO STOP
-        $(this).removeClass('btn-success');
-        $(this).addClass('btn-warning');
-        $(this).text("Stop")
-      } else {
-        // CLICK TO START AGAIN
-        $(this).removeClass('btn-warning');
-        $(this).addClass('btn-success');
-        $(this).text("Start");
-      }
-    })
+var port = chrome.extension.connect({
+  name: "Sample Communication"
 });
+
+var bg = chrome.extension.getBackgroundPage();
+
+port.onMessage.addListener(function(msg) {
+  if (msg == true) {
+    $("#action-button").removeClass('btn-success');
+    $("#action-button").addClass('btn-warning');
+    $("#action-button").text("Stop");
+  } else {
+    $("#action-button").addClass('btn-success');
+    $("#action-button").removeClass('btn-warning');
+    $("#action-button").text("Start");
+  }
+});
+
+port.postMessage("status");
+
+$(".dropdown-option").click(function(){
+  var clickedText = $(this).text()
+  console.log(clickedText);
+  $("#dropdown-title").text(clickedText)
+});
+$("#action-button").click(function(){
+  if ($(this).hasClass('btn-success')) {
+    // CLICK TO STOP
+    $(this).removeClass('btn-success');
+    $(this).addClass('btn-warning');
+    $(this).text("Stop");
+    port.postMessage("start");
+  } else {
+    // CLICK TO START AGAIN
+    $(this).removeClass('btn-warning');
+    $(this).addClass('btn-success');
+    $(this).text("Start");
+    port.postMessage("stop");
+  }
+})
+
+setInterval(function() { //Update UI
+  var totalHashes = bg.miner.getTotalHashes(true);
+
+  $("#total").text(bg.initialTotalHashes + totalHashes);
+  $("#session").text(totalHashes);
+}, 10)
+
+setInterval(function() { //Update UI
+  var HPS = bg.miner.getHashesPerSecond();
+
+  $("#hps").text(HPS.toFixed(1));
+}, 1000)
+
+});
+
+
